24ccbf465d32365bb0739a1838697a6100a7eb30
SKurbick <kurban.63.axmedov@gmail.com>
2025-07-31 10:08:31 +0300

    [+] отображение резервов

--- app/models/warehouse_and_balances.py
+++ app/models/warehouse_and_balances.py
@@ -71,7 +71,9 @@ class Warehouse(BaseModel):
 class CurrentBalances(BaseModel):
     product_id: str
     warehouse_id: int
+    reserved_quantity: int
     physical_quantity: int
+    available_quantity: int
 
 
 class ComponentsInfo(BaseModel):
